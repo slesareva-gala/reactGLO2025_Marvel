73a3a68102b0891947ffae8b6c09e8dda087ce36
Галина Слесарева <galaslesareva@gmail.com>
2025-04-26 11:05:18 +0300

    Updates

---
+++ static/js/528.bbb16fbe.chunk.js
@@ -0,0 +1,8 @@
+"use strict";(self.webpackChunkmarvel=self.webpackChunkmarvel||[]).push([[528],{1425:(e,t,n)=>{n.d(t,{A:()=>c});var a=n(5464);const i=n.p+"static/media/error.42292aa12b6bc303ce99.gif";var s=n(579);const r=a.Ay.img`
+  display: block;
+  width: 250px;
+  height: 250px;
+  object-fit: contain;
+  margin: 0 auto;
+`,c=()=>(0,s.jsx)(r,{src:i,alt:"error"})},7651:(e,t,n)=>{n.r(t),n.d(t,{default:()=>r});var a=n(1591),i=n(1425),s=n(579);const r=()=>(0,s.jsxs)("div",{children:[(0,s.jsxs)(a.m,{children:[(0,s.jsx)("meta",{name:"description",content:"404 | Page not found"}),(0,s.jsx)("title",{children:"Page not found"})]}),(0,s.jsx)(i.A,{}),(0,s.jsx)("h2",{style:{textAlign:"center",fontWeight:"bold",fontSize:"24px"},children:"404 Page Not Found"})]})}}]);
+//# sourceMappingURL=528.bbb16fbe.chunk.js.map
